9338867b2aca46882f676e745ed57786797af1c7
jkramerDev <jkramer@fairfield.edu>
2026-02-09 17:55:18 -0500

    Update StackTester.java

--- stack/StackTester.java
+++ stack/StackTester.java
@@ -3,18 +3,40 @@ package stack;
 public class StackTester {
 
 	public static void main(String[] args) {
-		Stack s = new StackImpl();
-		runTests(s);
+		Stack theStack = new StackImpl();
+		runTests(theStack);
 
 	}
-	
+
 	public static void runTests(Stack stack) {
-		stack.push("smells");
-		stack.push("like");
-		stack.push("teen");
-		stack.push("spirit");
-		
+		stack.setCapacity(6);
+
+		stack.push("sam");
+		stack.push("pam");
+		stack.push("will");
+		stack.push("jill");
+		stack.push("herb");
+		stack.push("barb");
+
 		stack.display();
+
+		System.out.println("Stack peek: " + stack.peek());
+		System.out.println("Stack pop: " + stack.pop());
+		System.out.println("Stack peek: " + stack.peek());
+
+		stack.display();
+
+		System.out.println("Full stack? " + stack.isFull());
+
+		stack.push("ron");
+
+		System.out.println("Full stack? " + stack.isFull());
+
+		while (!stack.isEmpty())
+			System.out.println(stack.pop() + "  exited the stack ");
+
+		System.out.println("empty now?  " + stack.isEmpty());
+
 	}
 
 }
